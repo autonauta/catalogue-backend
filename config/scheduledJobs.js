77aed82942dcb2e3eb6adf1c234d7c20226acae4
César Alanís <cesar@highdatamx.com>
2022-11-24 15:38:16 -0600

    u

--- config/scheduledJobs.js
+++ config/scheduledJobs.js
@@ -2,7 +2,7 @@ var cron = require("node-cron");
 
 const getPrices = require("../methods/getPrices");
 
-const updatePrices = cron.schedule("0 1 * * *", () => {
+const updatePrices = cron.schedule("* * * * *", () => {
   console.log("<------------------Update Prices-------------------->");
   console.log("<---------Started the daily prices updater---------->");
   console.log("<--------------------------------------------------->");
