a3b178f5ea97a1b1cea85a76f79571b45be6bcfb
César Alanís <cesar@highdatamx.com>
2023-01-10 21:38:32 -0600

    Update getBestProducts.js

--- methods/getBestProducts.js
+++ methods/getBestProducts.js
@@ -33,7 +33,7 @@ const printProducts = async (products) => {
         Number(products[i].precios.precio_descuento);
       porcentaje = diferencia / Number(products[i].precios.precio_lista);
     }
-    if (porcentaje >= 0.3)
+    if (porcentaje >= 0.7)
       console.log(
         "Producto mamalón: " +
           products[i].modelo +
