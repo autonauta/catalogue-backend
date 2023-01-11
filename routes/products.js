afbe63bc9d81dfb62d7b24eca29f1c9d8160f5c9
César Alanís <cesar@highdatamx.com>
2023-01-10 18:45:43 -0600

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const {Product} = require("../models/Product");
+const { Product } = require("../models/Product");
 
-
-router.get("/", async(req,res)=>{
-  const products = await Product.find({});
-  if(!products) {
-    res.status(400).send({error: true, message: "An error ocurred while getting the products info, try again later."});
+const printProducts = async (products) => {
+  for (let i = 0; i < products.length; i++) {
+    console.log(products[i]);
   }
+};
+
+router.get("/", async (req, res) => {
+  const products = await Product.find({});
+  if (!products) {
+    res
+      .status(400)
+      .send({
+        error: true,
+        message:
+          "An error ocurred while getting the products info, try again later.",
+      });
+  }
+  printProducts(products);
   res.send(products);
-})
+});
 //create new product in the DB
 router.post("/new", async (req, res) => {
   const name = req.body.name;
@@ -17,23 +29,23 @@ router.post("/new", async (req, res) => {
   const price = req.body.price;
   const category = req.body.category;
   const newProduct = new Product({
-    name, sysId, price, category
+    name,
+    sysId,
+    price,
+    category,
   });
-  try{
+  try {
     const product = await newProduct.save();
-    if(!product) {
+    if (!product) {
       console.log("No product saved");
-      res.send({error: true, message: "No product saved"});
-    }else{
+      res.send({ error: true, message: "No product saved" });
+    } else {
       console.log("Product saved");
       res.send(product);
     }
-  
-  }catch(err){
+  } catch (err) {
     res.status(400).send(err);
   }
 });
-
-
 
 module.exports = router;
