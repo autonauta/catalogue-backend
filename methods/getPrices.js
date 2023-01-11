ae7ae25817b3a66c8b8cc83cf25aad15c7fea40e
César Alanís <cesar@highdatamx.com>
2023-01-10 18:56:40 -0600

    Update getPrices.js

--- methods/getPrices.js
+++ methods/getPrices.js
@@ -25,6 +25,7 @@ const getPrices = async () => {
     if (!syscomProducts) {
       console.log("No products received from syscom.mx");
     } else {
+      console.log(syscomProducts);
       //printProducts(syscomProducts);
       updateProducts(syscomProducts);
     }
@@ -47,7 +48,7 @@ const updateProducts = async (products) => {
     };
     let productCreated = await Product.findOneAndUpdate(filter, update);
     productCreated = await Product.findOne(filter);
-    if (!productCreated.price === products[i].price) {
+    if (!(productCreated.price === products[i].price)) {
       console.log("Product " + products[i].producto_id + " was not updated");
     } else updateCounter++;
   }
