a54752bcae5b55ce835cc9dc8872c68c5f4fcbfa
César Alanís <cesar@highdatamx.com>
2023-01-20 14:37:19 -0600

    Update scheduledJobs.js

--- config/scheduledJobs.js
+++ config/scheduledJobs.js
@@ -2,7 +2,7 @@ var cron = require("node-cron");
 const getPrices = require("../methods/getPrices");
 
 //Update prices of all products in the data base every 4 hours
-const updatePrices = cron.schedule("0 * * * *", () => {
+const updatePrices = cron.schedule("* * * * *", () => {
   console.log("<------------------Update Prices-------------------->");
   console.log("<---------Started the daily prices updater---------->");
   console.log("<--------------------------------------------------->");
