6f12449549cffdd04bb29a0632e453d2486d5cd4
César Alanís <cesar@highdatamx.com>
2023-01-20 16:27:01 -0600

    Update diy.js

--- routes/diy.js
+++ routes/diy.js
@@ -48,4 +48,52 @@ router.get("/funnel", async (req, res) => {
 
   res.send(funnel);
 });
+//Endpoint for crreating new Funnels
+router.post("/funnel/new", async (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      error: true,
+      message: "No se recibieron datos.",
+    });
+    return;
+  }
+  const {
+    title,
+    subtitle,
+    image,
+    video,
+    buttonOne,
+    buttonTwo,
+    features,
+    productId,
+  } = req.body;
+  if (
+    !title ||
+    !subtitle ||
+    !image ||
+    !video ||
+    !buttonOne ||
+    !buttonTwo ||
+    !features ||
+    !productId
+  ) {
+    res.status(400).send({
+      error: true,
+      message: "No estan completos los datos.",
+    });
+    return;
+  }
+  const newFunnel = new Funnel({
+    title,
+    subtitle,
+    image,
+    video,
+    buttonOne,
+    buttonTwo,
+    features,
+    productId,
+  });
+  const saved = await newFunnel.save();
+  console.log(saved);
+});
 module.exports = router;
