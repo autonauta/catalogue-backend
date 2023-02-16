a89ccf3eaa5548786e357af5995052a33e10f50b
César Alanís <cesar@highdatamx.com>
2023-02-16 17:23:45 -0600

    Update syscom.js

--- routes/syscom.js
+++ routes/syscom.js
@@ -4,10 +4,10 @@ const config = require("config");
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-router.post("/estados", async (req, res) => {
+router.post("/locations", async (req, res) => {
   const { postal_code } = req.body;
   try {
-    const sysResponse = await fetch(
+    const estadoRaw = await fetch(
       config.get("SYSCOM_URL") + "carrito/estados/" + postal_code,
       {
         method: "GET",
@@ -17,10 +17,22 @@ router.post("/estados", async (req, res) => {
         },
       }
     );
-    const response = await sysResponse.json();
-    if (response.error) throw { message: response.message };
-    else if (response.estado)
-      res.send({ estado: response.estado[0].codigo_estado });
+    const estado = await estadoRaw.json();
+    if (estado.error) throw { message: estado.message };
+    const coloniesRaw = await fetch(
+      config.get("SYSCOM_URL") + "carrito/colonias/" + postal_code,
+      {
+        method: "GET",
+        headers: {
+          Authorization: config.get("SYSCOM_AUTH"),
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const colonies = await coloniesRaw.json();
+    if (colonies.error) throw { message: colonies.message };
+    console.log({ colonies, estado });
+    res.send({ colonies, estado });
   } catch (error) {
     console.log("Error: ", error);
     res.send({ error: true, message: error.message });
