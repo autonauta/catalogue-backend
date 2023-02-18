e544c1f914faa627a179fa62f557af1cc71fc940
César Alanís <cesar@highdatamx.com>
2023-02-17 21:28:05 -0600

    Update whatsapp.js

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.post("/send-message", async (req, res) => {
   const { num, message } = req.body;
   const whatsappClient = await req.app.get("whatsappClient");
-  whatsappClient.sendMessage("521" + num + "@c.us", message);
+  await whatsappClient.sendMessage("521" + num + "@c.us", message);
+  res.send({ error: "", message: "Mensaje enviado" });
 });
 module.exports = router;
