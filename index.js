361f4d7674ff8b16abecbee918b760e4cbd5c49f
César Alanís <cesar@highdatamx.com>
2023-02-17 20:44:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ client.on("qr", (qr) => {
 
 client.on("ready", () => {
   console.log("Whatsapp-web connected properly");
+  client.sendMessage("5214421818265@c.us", "Whatsap-web server just started!");
 });
 client.on("message", (message) => {
   console.log(message.body);
