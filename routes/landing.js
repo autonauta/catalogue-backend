1f4e34cba8430526952b0ec9d54ff750229b469c
César Alanís <cesar@highdatamx.com>
2024-03-07 21:17:11 -0600

    Update landing.js

--- routes/landing.js
+++ routes/landing.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { Customer } = require("../models/Customer");
 
 router.post("/contacto", async (req, res) => {
   const { nombre, telefono, correo, mensaje } = req.body;
@@ -9,6 +10,32 @@ router.post("/contacto", async (req, res) => {
       message: "No estan completos los datos.",
     });
     return;
+  }
+  const customer = await Customer.find({ correo });
+  if (customer) {
+    res.status(400).send({
+      error: true,
+      message: "Ya existe un usuario con ese correo.",
+    });
+  } 
+  else if (!customer) {
+    const newCustomer = new Customer({
+      nombre,
+      telefono,
+      correo,
+    });
+    try {
+      const product = await newProduct.save();
+      if (!product) {
+        console.log("No product saved");
+        res.send({ error: true, message: "No product saved" });
+      } else {
+        console.log("Product saved");
+        res.send(product);
+      }
+    } catch (err) {
+      res.status(400).send(err);
+    }
   }
   /* const newProduct = new Product({
     name,
