82a91f8c20ef86b3580fb2cbc83714dbc0c6c64a
César Alanís <cesar@highdatamx.com>
2024-03-11 17:38:17 -0600

    Update getConsumption.js

--- methods/getConsumption.js
+++ methods/getConsumption.js
@@ -6,7 +6,7 @@ const getConsumption = async (folder, file) => {
   // Construct a path to myfile.txt relative to script.js
   const filePath = path.join(__dirname, "..", folder, file);
   let dataBuffer = fs.readFileSync(filePath);
-  let pdf = await pdf(dataBuffer);
-  console.log("PDF dATA", pdf);
+  let processedPDF = await pdf(dataBuffer);
+  console.log("PDF dATA", processedPDF);
 };
 module.exports.getConsumption = getConsumption;
