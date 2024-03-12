358e77526097b6593f36158a9e1cea7b1b116fcb
César Alanís <cesar@highdatamx.com>
2024-03-12 14:56:15 -0600

    Update createProyect.js

--- methods/createProyect.js
+++ methods/createProyect.js
@@ -11,7 +11,6 @@ const inversores = [
   { modelo: "Inversor 6000W", potencia: 6000 },
   { modelo: "Inversor 10000W", potencia: 10000 },
   { modelo: "Inversor 36000W", potencia: 36000 },
-  { modelo: "Inversor 50000W", potencia: 50000 },
 ];
 
 const getMaxConsumption = async (text) => {
