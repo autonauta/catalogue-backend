848e9eec0f4e46fb90af23297745639e5e94eeda
César Alanís <cesar@highdatamx.com>
2024-03-15 15:23:21 -0600

    Update createProyect.js

--- methods/createProyect.js
+++ methods/createProyect.js
@@ -22,7 +22,7 @@ const createProyect = async (data) => {
   //Calcular el inversor
   let inversoresRequeridos = await getInversores(data.consumo);
   //Calcular cuantos strings
-  let stringsRequeridos = await getStrings(panelesRequeridos.cantidad);
+  let stringsRequeridos = await getStrings(panelesRequeridos);
   //Calcular el cable
   let cablesRequeridos = await getCables(stringsRequeridos.totalStrings);
   //Calcular soportería
