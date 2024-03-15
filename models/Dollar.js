b53796613dc569febd91be4f0703b67ed0a04a5e
César Alanís <cesar@highdatamx.com>
2024-03-15 15:10:59 -0600

    Update Dollar.js

--- models/Dollar.js
+++ models/Dollar.js
@@ -5,8 +5,6 @@ const dollarSchema = new mongoose.Schema({
   lastUpdate: { type: String },
 });
 
-let crmDB = mongoose.connection.useDb("crm");
-
-const Dollar = crmDB.model("Dollar", dollarSchema);
+const Dollar = mongoose.model("Dollar", dollarSchema);
 
 module.exports.Dollar = Dollar;
